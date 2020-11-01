1f06930d276d289d6e43cda1b8de2639ec402b7c
goutam <goutam@elogist.in>
2020-11-02 00:42:30 +0530

    hello

--- src/main/java/com/nagarro/model/Message.java
+++ src/main/java/com/nagarro/model/Message.java
@@ -4,6 +4,14 @@ public class Message {
 	private void psvm() {
 		int a=10;
 		System.out.println(a);
+		int b,c,d=0;
+		c=d;
+		try {
+			System.out.println("Hello");
+			
+		}catch(Exception e){
+			System.out.println("Hello A");
+		}
 
 	}
 
