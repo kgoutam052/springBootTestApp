42bc57c6868a3ea837b0486419b5a17abaec7377
goutam <goutam@elogist.in>
2020-11-02 00:02:53 +0530

    wh-8520- first time file update

---
+++ src/main/java/com/nagarro/Test.java
@@ -0,0 +1,13 @@
+package com.nagarro;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Test implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
